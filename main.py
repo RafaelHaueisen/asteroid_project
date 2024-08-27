e400044afd16b00e69b99dc03312a3392c32f0b2
RafaelHaueisen <rafaelhaueisenb@gmail.com>
2024-08-27 15:07:08 -0300

    Open black screen when running the program

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from constants import *
 
 
 def main():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}', f'Screen height: {SCREEN_HEIGHT}')
 
