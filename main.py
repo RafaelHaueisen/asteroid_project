4fc123121b76e53d13b5babb98ec99b6681ff43b
RafaelHaueisen <rafaelhaueisenb@gmail.com>
2024-08-28 13:56:25 -0300

    Updated main to handle asteroid groups

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame 
 from player import Player
 from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     updatable_group = pygame.sprite.Group()
     drawable_group = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
     Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (asteroid_group, updatable_group, drawable_group)
+    AsteroidField.containers = (updatable_group)
     body_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroid_field = AsteroidField()
     
     while True:
         for event in pygame.event.get():
@@ -20,9 +26,9 @@ def main():
 
         for updatable in updatable_group:
             updatable.update(dt)
+        screen.fill("black")
         for drawable in drawable_group:
             drawable.draw(screen)
-        screen.fill("black")
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
