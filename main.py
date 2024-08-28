a92492de3bceb0cec042610eee2841bb7cb7eadc
RafaelHaueisen <rafaelhaueisenb@gmail.com>
2024-08-28 10:15:03 -0300

    Added a player instance to main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, player
 from constants import *
 
 
@@ -7,15 +7,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    body_player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         screen.fill("black")
+        body_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
